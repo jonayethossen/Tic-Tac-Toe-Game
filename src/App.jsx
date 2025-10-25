0f783c3e7e3cf775fde4a39207f273c0396ca813
jonayethossen <jonayethossen8921@gmail.com>
2025-10-25 18:50:49 +0600

    Pass Data Board to Square function using props

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@
 //   ->Board
 //     ->  Square
 //   -> History
-function Square() {
+function Square({ value }) {
   return (
     <button className="text-5xl font-bold border border-2 m-2 h-20 w-20">
-      X
+      {value}
     </button>
   );
 }
@@ -14,19 +14,19 @@ export default function Board() {
   return (
     <>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
       </div>
     </>
   );
