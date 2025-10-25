09f2bc8c97bc5df60b6423d1df193df11200e266
jonayethossen <jonayethossen8921@gmail.com>
2025-10-25 18:46:38 +0600

    Create Square Function

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,32 @@
 //   ->Board
 //     ->  Square
 //   -> History
+function Square() {
+  return (
+    <button className="text-5xl font-bold border border-2 m-2 h-20 w-20">
+      X
+    </button>
+  );
+}
 
-export default function App() {
+export default function Board() {
   return (
     <>
-      <div>Tic tac toe</div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   );
 }
